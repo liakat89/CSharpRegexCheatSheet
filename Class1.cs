7b9bbaf7121d550391d22bb8963c89c36b71a783
Liakat Hossain <liakat89@gmail.com>
2023-03-08 20:41:59 +0100

    Create Initial Regex CheatSheet
    This is Initial Regex CheatSheet list

---
+++ Class1.cs
@@ -0,0 +1,28 @@
+namespace MyNameSpace.MyFolder
+{
+    public class Class1
+    {
+        /*
+            * ^ - Starts with
+            * $ - Ends with
+            * [] - Range
+            * () - Group
+            * . - Single character once
+            * + - one or more characters in a row
+            * ? - optional preceding character match
+            * \ - escape character
+            * \n - New line
+            * \d - Digit
+            * \D - Non-digit
+            * \s - White space
+            * \S - non-white space
+            * \w - alphanumeric/underscore character (word chars)
+            * \W - non-word characters
+            * {x,y} - Repeat low (x) to high (y) (no "y" means at least x, no ",y" means that may)
+            * (x|Y) - Alternative - x or y
+            * 
+            * [^x] - Anything but x (where x is whatever character you want)
+
+            */
+    }
+}
